620200a8a0cb43e8d70d53fd753e54c5da4d9aa7
Samet Eyisan <sameteyisannn@gmail.com>
2023-06-30 21:21:06 +0300

    real ads ids implemented

--- lib/utils/helper.dart
+++ lib/utils/helper.dart
@@ -25,11 +25,11 @@ class Helper {
   }
 
   static String get getBannerAdUnitId => Platform.isAndroid
-      ? 'ca-app-pub-3940256099942544/6300978111'
+      ? 'ca-app-pub-4589290119610129/6130021704'
       : 'ca-app-pub-3940256099942544/2934735716';
 
   static String get getInterstitialAdUnitId => Platform.isAndroid
-      ? 'ca-app-pub-3940256099942544/1033173712'
+      ? 'ca-app-pub-4589290119610129/3543719950'
       : 'ca-app-pub-3940256099942544/4411468910';
 
   static BannerAd get getAd {
